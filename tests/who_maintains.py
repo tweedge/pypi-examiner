d4c490ce641a5c26fc9da5ccdb051f2dd909b422
Chris Partridge <chris@partridge.tech>
2023-02-12 01:15:18 -0500

    Minor fix, naming issue in test

--- tests/who_maintains.py
+++ tests/who_maintains.py
@@ -2,14 +2,14 @@ import pytest
 from pypi_examiner import examiner
 
 
-def test_user_exists():
+def test_package_exists():
     pypi = examiner()
     result = pypi.who_maintains("unishox2_py3")
 
     assert result == ["tweedge"]
 
 
-def test_user_does_not_exist():
+def test_package_does_not_exist():
     pypi = examiner()
     result = pypi.who_maintains("httpxfaster")
 
